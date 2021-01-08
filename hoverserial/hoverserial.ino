3b2f70f1cf30615dcb8048c1940edad86f853ed2
Robo Durden <roland@robosoft.de>
2021-01-08 15:59:52 +0100

    now testing min speed

---
+++ hoverserial/hoverserial.ino
@@ -0,0 +1,44 @@
+#define DEBUG_HOVER
+
+#include "hoverFeru.h"  // uncomment if SERIAL_ROBO is not active in config.h or you have still the original Feru firmware on the controller :-)
+//#include "hoverRobo.h"
+
+#define TIME_SEND           500         // [ms] Sending time interval
+#define SPEED_MAX_TEST      100         // [-] Maximum speed for testing
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Arduino example code");
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  setupHover();
+}
+
+unsigned long iTimeSend = 0;
+int iTestMax = SPEED_MAX_TEST;
+int iTest = 0;
+
+void loop() 
+{
+  unsigned long timeNow = millis();
+
+  // Check for new received data
+  HoverReceive();
+
+  // Send commands
+  if (iTimeSend > timeNow) 
+    return;
+    
+  iTimeSend = timeNow + TIME_SEND;
+  //HoverSend(0, SPEED_MAX_TEST - 2*abs(iTest));
+  HoverSend(0, SPEED_MAX_TEST);
+  //Send(0, (timeNow % 10000 > 2000) ? SPEED_MAX_TEST : 0);
+
+  // Calculate test command signal
+  iTest += 1;
+  if (iTest > iTestMax) iTest = -iTestMax;
+
+  // Blink the LED
+  digitalWrite(LED_BUILTIN, (timeNow%2000)<1000);
+}
