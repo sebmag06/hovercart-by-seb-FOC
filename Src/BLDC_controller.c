ae85d57131d8bf61560424f3bcb5de807dae1cf4
Robo Durden <roland@robosoft.de>
2021-01-09 11:43:56 +0100

    tes

--- Src/BLDC_controller.c
+++ Src/BLDC_controller.c
@@ -1231,9 +1231,9 @@ void BLDC_controller_step(RT_MODEL *const rtM)
   }
 
 //#ifdef SPD_MODE_MAX
-  if (ABS(Switch2) > ABS(Switch2OtherMot)) Switch2 = Switch2OtherMot;   // apply outer motor speed
-  Switch2OtherMot = Switch2; // next call will be other/opposite motor :-)
   Switch2Test = Switch2;
+  if (ABS(Switch2) < ABS(Switch2OtherMot)) Switch2 = (Switch2+Switch2OtherMot)/2;   // apply outer motor speed
+  Switch2OtherMot = Switch2; // next call will be other/opposite motor :-)
 //#endif
 
   /* End of Switch: '<S13>/Switch2' */
